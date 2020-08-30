88125f710f2fe1cde94ec58d3e59d19539f3c87d
Netiel <j.diazi@uniandes.edu.co>
2020-08-30 17:16:33 -0500

    version mejorada insersiont, hay que hacer arreglos para que todos los metodos funciones

--- App/app.py
+++ App/app.py
@@ -1,3 +1,10 @@
+from App.modulos import *
+from DataStructures import listiterator as it
+from ADT import list as lt
+import config as cf
+import sys
+import os
+
 """
  * Copyright 2020, Departamento de sistemas y Computación, Universidad de Los Andes
  * 
@@ -25,56 +32,6 @@
   Este módulo es una aplicación básica con un menú de opciones para cargar datos, contar elementos, y hacer búsquedas sobre una lista .
 """
 
-# sorting
-
-# estructuras
-from DataStructures import liststructure as list_a
-from time import process_time
-from DataStructures import listiterator as it
-from ADT import list as lt
-import config as cf
-import sys
-import csv
-from Sorting.insertionsort import insertion_rank_mod as insertionSort
-from Sorting.mergesort import mergesort
-from Sorting.quicksort import quickSort
-from Sorting.selectionsort import selectionSort_n_rank as selectionSort
-from Sorting.shellsort import shellSort
-
-
-
-def loadCSVFile(file, tipo_lista, sep=";"):
-    """
-    Carga un archivo csv a una lista
-    Args:
-        file
-            Archivo csv del cual se importaran los datos
-        sep = ";"
-            Separador utilizado para determinar cada objeto dentro del archivo
-        tipo_lista
-            Define el tipo de lista para almacenar los datos entre "ARRAY_LIST" y
-            "SINGLE_LINKED"
-        Try:
-        Intenta cargar el archivo CSV a la lista que se le pasa por parametro, si encuentra algun error
-        Borra la lista e informa al usuario
-    Returns: None  
-    """
-    lst = lt.newList(tipo_lista)  # Usando implementacion linkedlist
-    print("Cargando archivo ....")
-    t1_start = process_time()  # tiempo inicial
-    dialect = csv.excel()
-    dialect.delimiter = sep
-    try:
-        with open(file, encoding="utf-8-sig") as csvfile:
-            spamreader = csv.DictReader(csvfile, dialect=dialect)
-            for row in spamreader:
-                lt.addLast(lst, row)
-    except:
-        print("Hubo un error con la carga del archivo")
-    t1_stop = process_time()  # tiempo final
-    print("Tiempo de ejecución ", t1_stop - t1_start, " segundos")
-    return lst
-
 
 def printMenu():
     """
@@ -89,68 +46,6 @@ def printMenu():
     print("0- Salir")
 
 
-def countElementsFilteredByColumn(criteria, column, lst):
-    """
-    Retorna cuantos elementos coinciden con un criterio para una columna dada  
-    Args:
-        criteria:: str
-            Critero sobre el cual se va a contar la cantidad de apariciones
-        column
-            Columna del arreglo sobre la cual se debe realizar el conteo
-        list
-            Lista en la cual se realizará el conteo, debe estar inicializada
-    Return:
-        counter :: int
-            la cantidad de veces ue aparece un elemento con el criterio definido
-    """
-    if lst['size'] == 0:
-        print("La lista esta vacía")
-        return 0
-    else:
-        t1_start = process_time()  # tiempo inicial
-        counter = 0
-        iterator = it.newIterator(lst)
-        while it.hasNext(iterator):
-            element = it.next(iterator)
-            # filtrar por palabra clave
-            if criteria.lower() in element[column].lower():
-                counter += 1
-        t1_stop = process_time()  # tiempo final
-        print("Tiempo de ejecución ", t1_stop - t1_start, " segundos")
-    return counter
-
-
-def countElementsByCriteria(criteria, column, lst):
-    """
-    Retorna la cantidad de elementos que cumplen con un criterio para una columna dada
-    """
-    return 0
-
-
-def orderElementsByCriteria(function, column: str, lst, orden, n_rank):
-    """
-    Retorna una lista con cierta cantidad de elementos ordenados por el criterio
-    function:
-        funcion de ordenamiento
-    colum:
-        nombre de la columna por la que se quiere ordenar
-    lst:
-        Estructura de lista que contiene los datos
-    orden:
-        funcion que indica la comparacion entre elementos, ej: lambda x,y: x > y
-    despligue:
-        cantidad de datos a desplegar
-
-    return:
-    """
-
-    def less_funtionnat(element1, element2):
-        return orden(float(element1[column]), float(element2[column]))
-
-    ord = function(lst, less_funtionnat, n_rank)
-
-    return ord
-
 def main():
     """
     Método principal del programa, se encarga de manejar todos los metodos adicionales creados
@@ -159,8 +54,15 @@ def main():
     Args: None
     Return: None 
     """
+    global lista_casting
+    global lista_details
     lista_casting = lt.newList()  # se require usar lista definida
     lista_details = lt.newList()
+
+    igual_str = lambda x, y: x == y
+    esta_al = lambda x, y: y in x
+    mayor = lambda x, y: float(x) > float(y)
+    menor = lambda x, y: float(x) < float(y)
     while True:
 
         printMenu()  # imprimir el menu de opciones en consola
@@ -168,50 +70,46 @@ def main():
         inputs = input('Seleccione una opción para continuar\n')
 
         if len(inputs) > 0:
-            if int(inputs[0]) == 1:  # opcion 1
-                file = "../Data/Movies/SmallMoviesDetailsCleaned.csv"
-                tipo_lista = input(
-                    "Ingrese el tipo de lista que quiere usar, 0 linked, 1 array: ")
+            if inputs[0].lower() == "q":  # opcion 1
+                tipo_lista = input("Ingrese el tipo de lista que quiere usar, 0 linked, 1 array: ")
                 if tipo_lista == "1":
                     tipo_lista = "ARRAY_LIST"
-                if tipo_lista == "2":
+                elif tipo_lista == "2":
                     tipo_lista = "SINGLE_LINKED"
                 valido = True
-                C1 = input(
-                    "¿Que archivos desea cargar? 1: prueba, 2: completos")
+                C1 = input("¿Que archivos desea cargar? 1: prueba, 2: completos")
                 if C1 == "1":
                     file_detail = "../Data/Movies/SmallMoviesDetailsCleaned.csv"
                     file_cast = "../Data/Movies/MoviesCastingRaw-small.csv"
 
-                elif int(C1) == 2:
+                elif C1 == "2":
                     file_detail = "../Data/Movies/AllMoviesDetailsCleaned.csv"
                     file_cast = "../Data/Movies/AllMoviesCastingRaw.csv"
+
                 else:
                     valido = False
                     print("Opcion invalida")
 
                 if valido:
-                    c1_1 = input("Datos del elenco? 0 o 1: ")
-                    if c1_1 == "1":
-                        print(file_cast, lista_casting)
-                        # llamar funcion cargar datos
-                        lista_casting = loadCSVFile(file_cast, tipo_lista)
-                        print("Datos cargados, " +
-                              str(lt.size(lista_casting)) + " elementos cargados")
-                        # llamar funcion cargar datos
-                    c1_2 = input("Datos de la pelicula? 0 o 1: ")
-                    if c1_2 == "1":
-                        print(file_detail, lista_details)
-                        lista_details = loadCSVFile(file_detail, tipo_lista)
-                        print("Datos cargados, " +
-                              str(lt.size(lista_details)) + " elementos cargados")
+                    print(file_cast, lista_casting)
+                    # llamar funcion cargar datos
 
-            elif int(inputs[0]) == 2:  # opcion 2
-                """
-                if ==None or lista['size']==0: #obtener la longitud de la lista
-                    print("La lista esta vacía")    
-                else: print("La lista tiene ",lista['size']," elementos")
-                """
+                    lista_casting = loadCSVFile(file_cast, tipo_lista)
+                    print("Datos cargados, " + str(lt.size(lista_casting)) + " elementos cargados")
+                    # llamar funcion cargar datos
+                    print(file_detail, lista_details)
+                    lista_details = loadCSVFile(file_detail, tipo_lista)
+                    print("Datos cargados, " + str(lt.size(lista_details)) + " elementos cargados")
+
+            elif int(inputs[0]) == 1:  # opcion 2
+                director = input("ingrese el nombre del director")
+                mayor_2 = lambda x, y: float(x) > y
+                global p1
+                p1 = Join_Extract_2_list_m_filter("id", lista_casting, lista_details, ["id", "director_name"],
+                                                  ["original_title", "vote_count", "vote_average"],
+                                                  [["director_name"], igual_str, director],
+                                                  [["vote_average"], mayor_2, 6])
+
             elif int(inputs[0]) == 3:  # opcion 3
                 # obtener la longitud de la lista
                 if lista_casting == None or lista_casting['size'] == 0:
@@ -241,8 +139,6 @@ def main():
                 if lista_details is None or lista_details['size'] == 0:
                     print("La lista esta vacía")
                 else:
-                    dict_ord = {"1": selectionSort, "2": insertionSort,
-                                "3": shellSort, "4": quickSort, "5": mergesort}
                     col_orden = input(
                         "Desea ordenar por AVERAGE: 1 o por COUNT: 2")
                     orden_str = input(
@@ -252,7 +148,7 @@ def main():
                     n_rank = int(
                         input("ingrese el numero de peliculas que quiere ver"))
                     t1 = process_time()
-                    funcion_orden = dict_ord[funcion_str]
+                    funcion_orden = 3
 
                     if orden_str == "0":
                         def orden(x, y):
